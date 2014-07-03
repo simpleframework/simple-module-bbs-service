b5ea065523bb51d76ee422db76e022ec04678056
simpleframework <cknet@126.com>
2014-07-03 18:03:36 +0800

    Signed-off-by: simpleframework <cknet@126.com>

--- src/main/java/net/simpleframework/module/bbs/impl/BbsTopicService.java
+++ src/main/java/net/simpleframework/module/bbs/impl/BbsTopicService.java
@@ -156,8 +156,9 @@ public class BbsTopicService extends AbstractContentService<BbsTopic> implements
 				super.onAfterUpdate(manager, columns, beans);
 
 				// 更新索引
-				if (columns == null || columns.length == 0 || ArrayUtils.contains(columns, "topic")
-						|| ArrayUtils.contains(columns, "content")) {
+				if (columns == null || columns.length == 0
+						|| ArrayUtils.contains(columns, "topic", true)
+						|| ArrayUtils.contains(columns, "content", true)) {
 					luceneService.doUpdateIndex(beans);
 				}
 			}
