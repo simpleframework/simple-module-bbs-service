779806bd22adcf02b4772eb7e8108e9ed885afca
CHENK <CHENK@CHENK-PC>
2015-09-24 18:23:00 +0800

    Signed-off-by: CHENK <CHENK@CHENK-PC>

--- src/main/java/net/simpleframework/module/bbs/impl/AbstractBbsService.java
+++ src/main/java/net/simpleframework/module/bbs/impl/AbstractBbsService.java
@@ -1,5 +1,7 @@
 package net.simpleframework.module.bbs.impl;
 
+import java.io.Serializable;
+
 import net.simpleframework.ctx.IModuleContext;
 import net.simpleframework.ctx.service.ado.db.AbstractDbBeanService;
 import net.simpleframework.module.bbs.IBbsContextAware;
@@ -10,8 +12,8 @@ import net.simpleframework.module.bbs.IBbsContextAware;
  * @author 陈侃(cknet@126.com, 13910090885) https://github.com/simpleframework
  *         http://www.simpleframework.net
  */
-public abstract class AbstractBbsService<T> extends AbstractDbBeanService<T> implements
-		IBbsContextAware {
+public abstract class AbstractBbsService<T extends Serializable> extends AbstractDbBeanService<T>
+		implements IBbsContextAware {
 
 	@Override
 	public IModuleContext getModuleContext() {
